5cf69904adf3b4a6b4787e618e308832ae11e25f
alaahosnii <alaahosnii1000@gmail.com>
2025-07-25 16:05:06 +0300

    Refactor import path in reduxHooks to use relative path for better module resolution

--- src/hooks/reduxHooks/reduxHooks.ts
+++ src/hooks/reduxHooks/reduxHooks.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from '@/redux/store';
+import type { RootState, AppDispatch } from '../../redux/store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
